3f1b33bebbb82cc6982bc5883b633a71c5f6993c
SMADA Dev Team <dev@smada.sch.id>
2026-08-12 16:38:41 +0700

    test perubahan

---
+++ app/Models/test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class test extends Model
+{
+    //
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'password',
+    ];
+}
