85d5a86d50f9c4b0ccd82d42585e515000eb3d84
SMADA Dev Team <dev@smada.sch.id>
2026-08-18 15:59:16 +0700

    add migrations

---
+++ database/migrations/2026_08_18_084721_add_activity_columns_to_galleries_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->string('activity_name', 150)->after('photo_url');
+            $table->date('activity_date')->after('activity_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropColumn(['activity_name', 'activity_date']);
+        });
+    }
+};
